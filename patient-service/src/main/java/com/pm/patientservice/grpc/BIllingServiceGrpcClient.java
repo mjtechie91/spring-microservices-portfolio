818859cb6110968899a1a37448290a96bdb6a876
Mahendra <mjtechie91@gmail.com>
2025-11-12 20:59:22 +0000

    Added billing service

---
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BIllingServiceGrpcClient.java
@@ -0,0 +1,46 @@
+package com.pm.patientservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingServiceGrpc;
+import billing.BillingResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+public class BillingServiceGrpcClient {
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+
+    // localhost:9001/BillingService/CreatePatientAccount
+    // aws.grps:123123/BillingService/CreatePatientAccount
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port:9001}") int serverPort
+    ){
+        log.info("Connecting to Billing Service GRPC service at {}:{}", serverAddress, serverPort);
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort).usePlaintext().build();
+
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public BillingResponse createBillingAccount(String patientId, String name, String email){
+
+        BillingRequest request = BillingRequest.newBuilder()
+                .setPatientId(patientId)
+                .setName(name)
+                .setEmail(email)
+                .build();
+        BillingResponse response = blockingStub.createBillingAccount(request);
+        log.info("Received response from Billing Service via GRPC: {}", response);
+        return response;
+
+    }
+}
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -4,6 +4,7 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -17,10 +18,12 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
+    private  BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository){
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient){
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients(){
@@ -34,6 +37,9 @@ public class PatientService {
             throw new EmailAlreadyExistsException("A patient with this email "+ patientRequestDTO.getEmail() + " already exists");
         }
         Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(patient.getId().toString(), patient.getName(), patient.getEmail());
+
         return PatientMapper.toDTO(patient);
     }
 
